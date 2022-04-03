9f19b27f2bdc74289e20686a71952ce652d4b5d6
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-03 12:52:53 +0900

    認証追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use App\Models\User;
 
 use Illuminate\Http\Request;
@@ -10,20 +11,27 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function login(Request $request) {
+        // Log::debug($request);
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
         if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+            // $request->session()->regenerate();
 
-            return redirect()->intended('dashboard');
+            // return redirect()->intended('dashboard');
+            $user = User::where('email', $request->email)->first();
+
+            return response($user, 200);
         }
 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
+        return response([
+            'MESSAGE' => '認証エラー',
+        ], 400);
+        // return back()->withErrors([
+        //     'email' => 'The provided credentials do not match our records.',
+        // ]);
     }
 
     function create(Request $request) {
