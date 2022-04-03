3e09be6a329587bf9c517b55de1b770a5795a961
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-04 00:00:06 +0900

    内容の抜粋を追加

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SearchContent;
+use Illuminate\Support\Facades\DB;
 
 class ContentController extends Controller
 {
     public function search(Request $request)
     {
-        $result = SearchContent::select('category', 'title')
+        $result = SearchContent::select('category', 'title', DB::raw("substring(content, locate('Java', content) - 50, 100) excerpt"))
         ->where('content', 'like', '%' . $request->keyword . '%')
         ->orderBy('category')
         ->orderBy('title')
