62d3fa4ff9db35df4bbfa81f20b02e2a7f3ce86a
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-03 21:43:14 +0900

    検索処理追加

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SearchContent;
 
 class ContentController extends Controller
 {
-    public function search()
+    public function search(Request $request)
     {
+        $result = SearchContent::where('content', 'like', '%' . $request->keyword . '%')
+        ->orderBy('category')
+        ->orderBy('title')
+        ->get();
 
+        return response($result, 200);
     }
 }
