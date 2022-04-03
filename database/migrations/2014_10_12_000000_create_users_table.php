9df155c7205b99e271b0bf46f4b7dcf640c67489
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-03 21:08:31 +0900

    カラム追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->nullable();
+            $table->integer('company_id')->nullable();;
             $table->rememberToken();
             $table->timestamps();
         });
