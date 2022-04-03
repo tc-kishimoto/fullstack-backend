395207fba94244248774045b4c19bc49548f3f1c
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-03 22:55:50 +0900

    limit追加

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -9,9 +9,11 @@ class ContentController extends Controller
 {
     public function search(Request $request)
     {
-        $result = SearchContent::where('content', 'like', '%' . $request->keyword . '%')
+        $result = SearchContent::select('category', 'title')
+        ->where('content', 'like', '%' . $request->keyword . '%')
         ->orderBy('category')
         ->orderBy('title')
+        ->limit(20)
         ->get();
 
         return response($result, 200);
