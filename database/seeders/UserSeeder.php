32675615c2ddd44b726951740d2fac57859c0870
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-29 21:35:50 +0900

    Seeder追加

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'login_id' => 'admin',
+            'role' => 1,
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
