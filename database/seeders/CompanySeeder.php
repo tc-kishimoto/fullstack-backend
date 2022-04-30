3f177ceba1ee18f5fcaa98a7868a1174b8d0b819
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-30 15:20:40 +0900

    会社とコース追加

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('companies')->insert([
+            'name' => 'T&Cテクノロジーズ株式会社',
+            'short_name' => 'T&C',
+        ]);
+        DB::table('companies')->insert([
+            'name' => '株式会社OSE',
+            'short_name' => 'OSE',
+        ]);
+    }
+}
---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('courses')->insert([
+            'name' => '2022年沖縄教室',
+        ]);
+        DB::table('courses')->insert([
+            'name' => '2022年日本橋教室A',
+        ]);
+    }
+}
