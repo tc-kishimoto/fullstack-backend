db4d1190c94646aaa0b022a70877e481366181fb
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-30 16:50:23 +0900

    キーワードが空白の場合の対応追加

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -9,14 +9,16 @@ use App\Models\Company;
 class CompanyController extends Controller
 {
     public function search(Request $request) {
-        $result = Company::select(
-            DB::raw("name link")
+        $model = Company::select(
+            "id","name"
+            , DB::raw("name link")
             , DB::raw("name link_title")
             , DB::raw("name explanation")
-        )
-        ->where(DB::raw("concat(name, short_name)"), 'like', '%' . $request->keyword . '%')
-        ->get();
-
+        );
+        if($request->has('keyword')) {
+            $model = $model->where(DB::raw("concat(name, short_name)"), 'like', '%' . $request->keyword . '%');
+        }
+        $result = $model->get();
         return response($result, 200);
     }
 }
