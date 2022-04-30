6506cacdec870343312a163633c6a1f18d49ce9f
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-04-30 10:55:19 +0900

    取得データ修正

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -16,7 +16,11 @@ class ContentController extends Controller
                 when instr(content, '$request->keyword') - 50 < 0 then instr(content, '$request->keyword')
                 else instr(content, '$request->keyword') - 50
             end
-            , 100) excerpt"))
+            , 100) explanation")
+            , DB::raw("concat('../images/index/', category, '.png') img_path")
+            , DB::raw("concat(category, '_', title, '.html') link")
+            , DB::raw("concat(category, '_', title) link_title")
+            )
         ->where('content', 'like', '%' . $request->keyword . '%')
         ->orderBy('category')
         ->orderBy('title')
