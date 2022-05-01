bb0294eca7b42ddb1c396a0416a4374527589367
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-01 18:28:33 +0900

    提出用のテーブル作成

---
+++ database/migrations/2022_05_01_092356_create_submissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('category');
+            $table->string('lesson_name');
+            $table->string('comment', 2000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submissions');
+    }
+};
