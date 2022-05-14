71007873547880f563c1d9d06e2178dd0d205e61
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-14 22:20:18 +0900

    リンク変更

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -25,12 +25,12 @@ class CompanyController extends Controller
         $model = Company::select(
             "id","name"
             , DB::raw("'' img_path")
-            , DB::raw("name link")
+            , DB::raw("concat('/html/companyDetail.html?id=', id) link")
             , DB::raw("name link_title")
             , DB::raw("name explanation")
         );
         if($request->has('keyword')) {
-            $model = $model->where(DB::raw("concat(name, short_name)"), 'like', '%' . $request->keyword . '%');
+            $model = $model->where(DB::raw("concat(name, ifnull(short_name, ''))"), 'like', '%' . $request->keyword . '%');
         }
         $result = $model->get();
         return response($result, 200);
