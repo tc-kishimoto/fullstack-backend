3372dff67bede213eadcf36212d8a18427ca306b
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-17 16:40:55 +0900

    権限の定数追加

---
+++ app/Consts/UserConst.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Consts;
+
+class UserConst
+{
+    public const ROLE_SYSTEM_ADMIN = 1;
+    public const ROLE_COMPANY_ADMIN = 2;
+    public const ROLE_COURSE_ADMIN = 3;
+    public const STUDENT = 4;
+}
