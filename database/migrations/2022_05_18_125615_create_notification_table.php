67b094958acfc895d0fb5d54b952545ced1b0e70
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-18 22:04:41 +0900

    通知用テーブル追加

---
+++ database/migrations/2022_05_18_125615_create_notification_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->id();
+            $table->integer('source_user_id');
+            $table->integer('target_user_id');
+            $table->string('target_table');
+            $table->string('target_id');
+            $table->string('conent');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+};
