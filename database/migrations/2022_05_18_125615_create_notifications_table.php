73a2b9778444ba049087b6517bbd899baea9458f
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-18 22:13:59 +0900

    テーブル名変更

--- database/migrations/2022_05_18_125615_create_notifications_table.php
+++ database/migrations/2022_05_18_125615_create_notifications_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('notification', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->integer('source_user_id');
             $table->integer('target_user_id');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notification');
+        Schema::dropIfExists('notifications');
     }
 };
