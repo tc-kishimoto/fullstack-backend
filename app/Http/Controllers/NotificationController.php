83b86d5df59a4d82103782f72bf84fb2bd8774c4
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-18 23:09:37 +0900

    最新10件を取得するように修正

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,7 +10,9 @@ class NotificationController extends Controller
     public function getNotification(Request $request)
     {
         $result = Notification::where('target_user_id', '=', $request->user()->id)
-        ->where('status', '=', 0)
+        // ->where('status', '=', 0)
+        ->orderByDesc('created_at')
+        ->limit(10)
         ->get();
         return response($result, 200);
     }
