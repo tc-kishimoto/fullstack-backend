b4599978e75369d3930c98e058bfdabaef758054
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-18 22:24:21 +0900

    通知のモデルとコントローラ追加

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    public function getNotification(Request $request)
+    {
+        $result = Notification::where('target_user_id', '=', $request->user()->id)
+        ->where('status', '=', 0)
+        ->get();
+        return response($result, 0);
+    }
+
+    public function updateNotificationStatus(Request $request)
+    {
+        Notification::where('target_user_id', $request->user()->id)
+        ->where('status', 0)
+        ->update(['status' => 1]);
+        return response([], 200);
+    }
+}
