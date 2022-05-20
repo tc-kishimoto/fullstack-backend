1d3850eb50c60797fa1708b99e6125f0c9441978
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-20 11:31:48 +0900

    fillableと属性追加

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sourse_user_id',
+        'target_user_id',
+        'target_table',
+        'target_id',
+        'content',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    public function getNotifyTitleAttribute()
+    {
+        switch($this->target_table) {
+            case 'submissions':
+                return '演習提出通知';
+            default :
+                return '';
+        }
+    }
 }
