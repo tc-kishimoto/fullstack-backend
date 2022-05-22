f5770d024184aa4290e96923115206c0fb86d859
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-22 13:54:00 +0900

    コメント用テーブル追加

---
+++ database/migrations/2022_05_22_134755_create_submission_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submission_comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('submission_id');
+            $table->string('comment', 2000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submission_comments');
+    }
+};
